a639ae58ec32a8523642c0d7a9dcad7818ae820e
7y1o <tdesu.webdesign@gmail.com>
2021-08-06 15:46:19 +0300

    Added controller initialization

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-module.exports.loop = () => {};
+const SpawnController = require('./controllers/spawn');
+const controller = new SpawnController();
+
+module.exports.loop = () => {
+    controller.update();
+};
